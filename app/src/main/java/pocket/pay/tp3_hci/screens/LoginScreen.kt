4d3c00dd6552c13f187baab5bcd9534b587330cc
Nicolas Kim <nkim@itba.edu.ar>
2024-11-17 01:38:02 -0300

    icons & register screen

--- app/src/main/java/pocket/pay/tp3_hci/screens/LoginScreen.kt
+++ app/src/main/java/pocket/pay/tp3_hci/screens/LoginScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -16,6 +18,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -91,22 +94,19 @@ fun LoginScreen() {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth(),
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        PasswordInputField(
+            password = password,
+            onPasswordChange = { password = it }
         )
 
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(27.dp))
 
         Button(
             onClick = { /* Lógica o nav de login */ },
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = "Login")
+            Text(text = "Login",
+                fontSize = 17.sp,)
         }
 
         Spacer(modifier = Modifier.height(13.dp))
@@ -119,7 +119,8 @@ fun LoginScreen() {
                 contentColor = Color.White
             )
         ) {
-            Text(text = "Register")
+            Text(text = "Register",
+                fontSize = 17.sp,)
         }
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -129,9 +130,42 @@ fun LoginScreen() {
             modifier = Modifier.fillMaxWidth()
         ) {
             Text(text = "Forgot your password?",
-                fontSize = 17.sp,)
+                fontSize = 19.sp,)
         }
     }
+}
+
+@Composable
+fun PasswordInputField(
+    password: String,
+    onPasswordChange: (String) -> Unit
+) {
+    // Variable de estado para controlar si la contraseña se muestra u oculta
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = password,
+        onValueChange = onPasswordChange,
+        label = { Text("Password") },
+        modifier = Modifier.fillMaxWidth(),
+        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val icon = if (isPasswordVisible) {
+                painterResource(id = R.drawable.ic_visibility_off)
+            } else {
+                painterResource(id = R.drawable.ic_visibility)
+            }
+
+            IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                Icon(
+                    painter = icon,
+                    contentDescription = if (isPasswordVisible) "Hide password" else "Show password",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/pocket/pay/tp3_hci/screens/RegisterScreen.kt
+++ app/src/main/java/pocket/pay/tp3_hci/screens/RegisterScreen.kt
@@ -1,2 +1,177 @@
 package pocket.pay.tp3_hci.screens
 
+import androidx.compose.material3.Icon
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import pocket.pay.tp3_hci.R
+import pocket.pay.tp3_hci.ui.theme.Purple
+
+@Composable
+fun RegisterScreen() {
+
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var mobileNumber by remember { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf(false) }
+    var isConfirmPasswordVisible by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Spacer(modifier = Modifier.height(33.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Sign ",
+                color = Color.Black,
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "up",
+                color = Purple,
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Spacer(modifier = Modifier.height(17.dp))
+
+        Text(
+            text = "Please fill out the information to create an account",
+            color = Color.Gray,
+            fontSize = 23.sp,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Name") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                val icon = if (isPasswordVisible) {
+                    painterResource(id = R.drawable.ic_visibility_off)
+                } else {
+                    painterResource(id = R.drawable.ic_visibility)
+                }
+                IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                    Icon(
+                        painter = icon,
+                        contentDescription = if (isPasswordVisible) "Hide password" else "Show password",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = { Text("Confirm Password") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = if (isConfirmPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            // keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                val icon = if (isPasswordVisible) {
+                    painterResource(id = R.drawable.ic_visibility_off)
+                } else {
+                    painterResource(id = R.drawable.ic_visibility)
+                }
+
+                IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                    Icon(
+                        painter = icon,
+                        contentDescription = if (isPasswordVisible) "Hide password" else "Show password",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = mobileNumber,
+            onValueChange = { mobileNumber = it },
+            label = { Text("Mobile Number") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+
+        Button(
+            onClick = { /* crear la cuenta */ },
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(containerColor = Purple, contentColor = Color.White)
+        ) {
+            Text(text = "Create my account",
+                fontSize = 19.sp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterScreenPreview() {
+    RegisterScreen()
+}
+
+
+
+
+
