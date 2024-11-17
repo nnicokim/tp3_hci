fdee599e6dc813e5e5b9660ebdcc12e2537a2d13
Nicolas Kim <nkim@itba.edu.ar>
2024-11-16 23:51:27 -0300

    changes in login screen

--- app/src/main/java/pocket/pay/tp3_hci/screens/LoginScreen.kt
+++ app/src/main/java/pocket/pay/tp3_hci/screens/LoginScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -31,9 +32,11 @@ fun LoginScreen() {
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.height(48.dp))
+
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
@@ -41,38 +44,38 @@ fun LoginScreen() {
             Image(
                 painter = painterResource(id = R.drawable.pplogo),
                 contentDescription = "Logo",
-                modifier = Modifier.size(64.dp)
+                modifier = Modifier.size(90.dp)
             )
-            Spacer(modifier = Modifier.width(8.dp))
+            Spacer(modifier = Modifier.width(15.dp))
             Text(
                 text = "Pocket ",
                 color = Color.Black,
-                fontSize = 24.sp,
+                fontSize = 45.sp,
                 fontWeight = FontWeight.Bold
             )
             Text(
                 text = "Pay",
                 color = Purple,
-                fontSize = 24.sp,
+                fontSize = 45.sp,
                 fontWeight = FontWeight.Bold
             )
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
         Text(
             text = "Your entire wallet in your pocket",
             color = Color.Gray,
-            fontSize = 16.sp,
+            fontSize = 30.sp,
             textAlign = TextAlign.Center
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(30.dp))
 
         Text(
             text = "Enter your username and password to access your account",
             color = Color.Black,
-            fontSize = 14.sp,
+            fontSize = 17.sp,
             textAlign = TextAlign.Center
         )
 
@@ -100,19 +103,33 @@ fun LoginScreen() {
         Spacer(modifier = Modifier.height(24.dp))
 
         Button(
-            onClick = { /* Lógica de login */ },
+            onClick = { /* Lógica o nav de login */ },
             modifier = Modifier.fillMaxWidth()
         ) {
             Text(text = "Login")
         }
 
+        Spacer(modifier = Modifier.height(13.dp))
+
+        Button(
+            onClick = { /* Lógica o nav de register */ },
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Purple,
+                contentColor = Color.White
+            )
+        ) {
+            Text(text = "Register")
+        }
+
         Spacer(modifier = Modifier.height(16.dp))
 
         TextButton(
-            onClick = { /* Lógica para recuperar contraseña */ },
+            onClick = { /* nav al screen para recuperar contraseña */ },
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = "Forgot your password?")
+            Text(text = "Forgot your password?",
+                fontSize = 17.sp,)
         }
     }
 }
