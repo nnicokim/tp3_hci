3d4dca52e9bd851e077aba2c4161cd9be317292a
hwapyoung <hkim@itba.edu.ar>
2024-11-26 13:07:54 -0300

    Balance fix attempt

--- app/src/main/java/pocket/pay/tp3_hci/states/AccountUiState.kt
+++ app/src/main/java/pocket/pay/tp3_hci/states/AccountUiState.kt
@@ -1,5 +1,6 @@
 package pocket.pay.tp3_hci.states
 
+import pocket.pay.tp3_hci.model.Balance
 import pocket.pay.tp3_hci.model.Card
 import pocket.pay.tp3_hci.model.Payment
 import pocket.pay.tp3_hci.model.User
@@ -11,7 +12,7 @@ data class AccountUiState(
     val error: Error? = null,
     val currentCard: Card? = null,
     val currentPayment: Payment? = null,
-    val currentBalance: Number = 0.0,
+    val currentBalance: Balance? = Balance(0.0),
 
     val payments: List<Payment>? = emptyList(),
     val cards: List<Card>? = emptyList(),
